b4c6744e6164f21a913cc6629cce363e25d0e841
Chris Maguire <cwmaguire@gmail.com>
2014-04-07 21:24:27 -0700

    Fix chainsaw icon, cleanup exploritory code
    The chainsaw icon now shows as the axe (close enough for now).
    
    Since this is now under source control I could gut all the exploritory
    code I added and all the debug stuff. Anything I need I can fish out of
    the commit logs.

--- chainsaw/chainsaw.lua
+++ chainsaw/chainsaw.lua
@@ -1,42 +1,12 @@
 local function addChainsaw(keyPressed)
-  print(keyPressed)
-  print(Keyboard.KEY_INSERT:toString())
-
   if keyPressed == Keyboard.KEY_INSERT then
-    print(""..keyPressed.." is equal to "..Keyboard.KEY_INSERT)
+    print("INSERT pressed: adding chainsaw")
     local player = getPlayer()
     local inv = player:getInventory()
 
-	  chainsaw = InventoryItemFactory.CreateItem("chainsaw.Chainsaw")
-    player:sendObjectChange('addItem', {item = chainsaw})
     inv:AddItem("chainsaw.Chainsaw")
     inv:AddItem("Base.Axe")
   end
 end
 
-local function sayKey(keyPressed)
-  print("Key is "..keyPressed)
-  print("Insert key is "..Keyboard.KEY_INSERT)
-  --local player = getPlayer():Say("Key is"..toString(keyPressed))
-end
-
-local function swingBatter()
-  print("****************************** swing batter")
-  getPlayer():Say("Swing batter!")
-  --getSpecifiedPlayer(0):Say("Swing batter! (specified player)")
-end
-
-local function saySomething()
-  print("********************************** saySomething() ****************************")
-end
-
-chainsawObject = {}
-chainsawObject.saySomething = function()
-  print("********************************* chainsawObject.saySomething() ************************")
-end
-
 Events.OnKeyPressed.Add(addChainsaw)
-Events.OnKeyPressed.Add(sayKey)
-Events.OnWeaponSwing.Add(swingBatter)
-Events.OnGameBoot.Add(saySomething)
-Events.OnGameBoot.Add(chainsawObject.saySomething)
