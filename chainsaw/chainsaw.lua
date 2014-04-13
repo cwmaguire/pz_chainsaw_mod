5bc1723f90cfa9d42d970a7a246b1ebc9c6ede91
Chris Maguire <cwmaguire@gmail.com>
2014-04-12 18:12:41 -0700

    Add silly chainsaw revving swing sound
    Yes, this is silly; it's a placeholder. :)
    
    Note: oggenc wouldn't encode raw with an EOF and
    and would crash so I arecord'ed into wav and
    oggenc'd the wav. Worked better.

--- chainsaw/chainsaw.lua
+++ chainsaw/chainsaw.lua
@@ -1,3 +1,13 @@
+-- What do I want to do?
+--
+-- 1) Have the chainsaw idle
+-- 2) Play an idle noise
+-- 3) Have the chainsaw swing
+-- 4) Have a swinging revving noise
+-- 5) Have idling use fuel
+-- 6) Have swining use fuel
+-- 7) Add fuel
+--
 local function addChainsaw(keyPressed)
   if keyPressed == Keyboard.KEY_INSERT then
     print("INSERT pressed: adding chainsaw")
