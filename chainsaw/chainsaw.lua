5db478d7d83de3678f29f73f890e8f1744a751e1
Chris Maguire <cwmaguire@gmail.com>
2014-04-14 22:04:54 -0700

    WIP: Add chainsaw fill menus
    I'm giving each chainsaw and each fuel can an age. In the "fill ..."
    menu I'm listing each chainsaw with the amount of fuel it has (and
    whether it's equipped) and then under each chainsaw I'm listing each
    petrol can with how much fuel it has.
    
    Once a user picks a chainsaw and some fuel then I'll subtract the fuel
    "age" from the petrol can and add it to the chainsaw.
    
    Empty petrol cans and full chainsaws won't show up in the menus.
    
    Petrol cans and chainsaws without their fuel "age" in the display will
    have it set when the user right clicks.
    
    I might go through and reset chainsaw and petrol can display names every
    few minutes or so, ... but I doubt it.

--- chainsaw/chainsaw.lua
+++ chainsaw/chainsaw.lua
@@ -11,6 +11,16 @@
 
 Chainsaw = {}
 Chainsaw.tick = 0;
+Chainsaw.chainsawName = "Chainsaw"
+Chainsaw.petrolCanName = "Gas Can"
+
+do
+  local twentyMinutes = 20 * 60
+  Chainsaw.chainsawFuelLimit = twentyMinutes
+
+  local oneHour = 60 * 60
+  Chainsaw.petrolCanFuel = oneHour
+end
 
 Chainsaw.addChainsaw = function(keyPressed)
   if keyPressed == Keyboard.KEY_INSERT then
@@ -20,7 +30,84 @@ Chainsaw.addChainsaw = function(keyPressed)
 
     inv:AddItem("chainsaw.Chainsaw")
     inv:AddItem("Base.Axe")
+    inv:AddItem("Base.PetrolCan")
   end
+end
+
+Chainsaw.isChainsaw = function(item)
+  return string.find(item.getDisplayName(), Chainsaw.chainsawName)
+end
+
+Chainsaw.isFull = function(chainsaw)
+  local twentyMinutes = 20 * 60
+  return chainsaw.getAge() < twentyMinutes
+end
+
+Chainsaw.getChainsawsNotFull = function(player)
+  local equippedItem = player.getPrimaryHandItem()
+  local equippedChainsaw
+  local chainsaws = {}
+  if Chainsaw.isChainsaw(equippedItem) and
+    not Chainsaw.isFull(equippedItem) then
+    equippedChainsaw = equippedItem
+  end
+
+  for item in player:getInventory():getItems() do
+    if Chainsaw.isChainsaw(item) and not Chainsaw.isFull(item) then
+      item.setDisplayName("Chainsaw [" .. item.getAge() .."]")
+      table.insert(chainsaws, equippedItem)
+    end
+  end
+
+  return equippedChainsaw, chainsaws
+end
+
+Chainsaw.isPetrolCan = function(item)
+  return string.find(item.getDisplayName(), "Gas Can")
+end
+
+Chainsaw.isPetrolCanEmpty = function(petrolCan)
+  return petrolCan.getAge() > 0
+end
+
+Chainsaw.getPetrolCansNotEmpty = function(player)
+  local petrolCans = {}
+  for item in player:getInventory():getItems() do
+    if Chainsaw.isPetrolCan(item) and
+      not Chainsaw.isPetrolCanEmpty(item) then
+      item.setDisplayName("Gas Can [".. item.getAge() .."]")
+      table.insert(petrolCans, equippedItem)
+    end
+  end
+
+  return petrolCans
+end
+
+Chainsaw.fillChainsaw = function(player, chainsaw, petrolCan)
+  local chainsawFuel = Chainsaw.getFuel(chainsaw)
+  local petrolCanFuel = Chainsaw.getPetrolCanFuel(petrolCan)
+
+  local fuelNeeded = Chainsaw.chainsawFuelLimit - chainsawFuel
+  local petrolLeft
+  if fuelNeeded <= petrolCanFuel then
+    fuelLeft = petrolCanFuel - fuelNeeded
+    chainsawFuel = Chainsaw.chainsawFuelLimit
+  else
+    fuelLeft = 0
+    chainsawFuel = chainsawFuel + petrolCanFuel
+  end
+  Chainsaw.setFuel(chainsaw, chainsawFuel)
+  Chainsaw.setPetrolCanFuel(petrolCan, fuelLeft)
+end
+
+Chainsaw.setFuel = function(chainsaw, fuel)
+  chainsaw:setAge(fuel)
+  chainsaw:setDisplayName("Chainsaw [" .. fuel .. "]")
+end
+
+Chainsaw.setPetrolCanFuel = function(petrolCan, fuel)
+  petrolCan:setAge(fuel)
+  petrolCan:setDisplayName(ChainSaw.petrolCanName .. "[" .. fuel .. "]")
 end
 
 Chainsaw.setIsChainsawEquipped = function()
@@ -34,17 +121,33 @@ Chainsaw.setIsChainsawEquipped = function()
 end
 
 Chainsaw.onEquipPrimary = function()
+  local item = nil
   Chainsaw.setIsChainsawEquipped()
   if Chainsaw.isChainsawEquipped then
     Chainsaw.tick = 0
     Chainsaw.isChainsawEquipped = true
     Chainsaw.playIdleSound()
+    Chainsaw.ensureUses()
+  end
+end
+
+Chainsaw.ensureAge = function()
+  local item = getPlayer():getPrimaryHandItem()
+  if item:getAge() <= 0 then
+    item:setAge(100.0)
   end
 end
 
 Chainsaw.playIdleSound = function()
   print("Chainsaw.playIdleSound: playing idle sound")
-  getSoundManager():PlaySound("chainsaw_idle", false, 1.0)
+  getSoundManager():PlaySound("chainsaw_idle", false, 0.5)
+end
+
+Chainsaw.use = function()
+  local player = getPlayer()
+  local item = player:getPrimaryHandItem()
+  item:setAge(item:getAge() - 1.0)
+  print("\"Using\" Chainsaw (lowering age)")
 end
 
 Chainsaw.onTick = function()
@@ -52,6 +155,9 @@ Chainsaw.onTick = function()
   if Chainsaw.tick % 120 == 0 and Chainsaw.isChainsawEquipped then
     print("Chainsaw.onTick: chainsaw is equipped; playing idle sound")
     Chainsaw.playIdleSound()
+  end
+  if Chainsaw.tick % 30 == 0 and Chainsaw.isChainsawEquipped then
+    Chainsaw.use()
   end
 end
 
